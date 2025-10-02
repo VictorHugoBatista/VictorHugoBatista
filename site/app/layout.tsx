5ca32bd5ffd5ab84f6a184925b9b17820bc6ae2f
Victor Hugo Batista <vct.bta@gmail.com>
2025-10-02 20:23:44 -0300

    Add jumbotron, text effects and more

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bitcount, Bitcount_Single, Doto } from 'next/font/google';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const _fontTitle = Bitcount({
+  subsets: ['latin'],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const _fontPage = Bitcount_Single({
+  subsets: ['latin'],
+});
+
+const _fontPre = Doto({
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="antialiased"
       >
         <div className="monitor">
           <div className="monitor-screen">
--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,14 +1,22 @@
-import { Bitcount, Bitcount_Single } from 'next/font/google';
+'use client';
 
-const _fontTitle = Bitcount({
-  subsets: ['latin'],
-});
-
-const _fontPage = Bitcount_Single({
-  subsets: ['latin'],
-})
+import { useState } from "react";
+import { colossal, useAsciiText } from "react-ascii-text";
 
 export default function Home() {
+  const [text] = useState(["Fullsatack", "React", "Vue", "Node", "PHP", "Java", "SQL", "NoSQL"]);
+  const asciiTextRef = useAsciiText({
+    animationCharacters: "▒░█",
+    animationCharacterSpacing: 1,
+    animationDelay: 2000,
+    animationDirection: "right",
+    animationInterval: 100,
+    animationLoop: true,
+    animationSpeed: 5,
+    font: colossal,
+    text,
+  });
+
   return (
     <div className="container mx-auto px-4">
       <main>
@@ -16,15 +24,26 @@ export default function Home() {
           <h1>Victor Hugo Batista</h1>
         </header>
         <section className="monitor-screen-content">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero pariatur dolor maiores a nesciunt autem reprehenderit maxime voluptates quidem id deserunt odio, ipsum officiis fuga, aperiam nobis aliquid repudiandae officia.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero pariatur dolor maiores a nesciunt autem reprehenderit maxime voluptates quidem id deserunt odio, ipsum officiis fuga, aperiam nobis aliquid repudiandae officia.
-          </p>
+          <div className="monitor-screen-jumbotron">
+            <pre className="text-white w-full mx-auto" ref={asciiTextRef} />
+          </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis magni ducimus vero voluptate tempore eum ex deserunt accusamus, harum ab temporibus pariatur cum officiis, ullam minima voluptatem doloremque id dolorum!</p>
+            <p>Voluptas iste magni aliquam voluptatem id odio dolores, excepturi, autem earum architecto, dolor beatae! Atque ipsam nam nisi, commodi repudiandae rerum, expedita fuga ad, maxime inventore explicabo tempore tempora quas.</p>
+            <p>Ex dolorum incidunt commodi at repellat suscipit! Maxime necessitatibus quidem repellendus vitae tenetur sunt explicabo? Hic quo necessitatibus voluptatum, autem sapiente ea voluptas. Inventore, asperiores deleniti veniam necessitatibus pariatur nihil.</p>
+            <p>Pariatur iusto voluptates eveniet ullam delectus? Rem tempore optio minima officia, hic veritatis neque totam deleniti soluta doloribus nisi laudantium? Magni recusandae consequuntur officia, quaerat harum porro mollitia officiis quisquam.</p>
+            <p>Saepe, ipsum maxime pariatur nisi ab molestiae omnis similique dolorem debitis nobis! Eveniet non expedita cum commodi doloribus quo a temporibus, reprehenderit architecto at, alias, omnis eos minus debitis sapiente.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis magni ducimus vero voluptate tempore eum ex deserunt accusamus, harum ab temporibus pariatur cum officiis, ullam minima voluptatem doloremque id dolorum!</p>
+            <p>Voluptas iste magni aliquam voluptatem id odio dolores, excepturi, autem earum architecto, dolor beatae! Atque ipsam nam nisi, commodi repudiandae rerum, expedita fuga ad, maxime inventore explicabo tempore tempora quas.</p>
+            <p>Ex dolorum incidunt commodi at repellat suscipit! Maxime necessitatibus quidem repellendus vitae tenetur sunt explicabo? Hic quo necessitatibus voluptatum, autem sapiente ea voluptas. Inventore, asperiores deleniti veniam necessitatibus pariatur nihil.</p>
+            <p>Pariatur iusto voluptates eveniet ullam delectus? Rem tempore optio minima officia, hic veritatis neque totam deleniti soluta doloribus nisi laudantium? Magni recusandae consequuntur officia, quaerat harum porro mollitia officiis quisquam.</p>
+            <p>Saepe, ipsum maxime pariatur nisi ab molestiae omnis similique dolorem debitis nobis! Eveniet non expedita cum commodi doloribus quo a temporibus, reprehenderit architecto at, alias, omnis eos minus debitis sapiente.</p>
         </section>
         <footer className="monitor-screen-footer">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero pariatur dolor maiores a nesciunt autem reprehenderit maxime voluptates quidem id deserunt odio, ipsum officiis fuga, aperiam nobis aliquid repudiandae officia.
+          <ul>
+            <li>Estilizado por Victor Hugo Batista</li>
+            <li><a about="blank" href="https://www.linkedin.com/in/victorhugobatista">LinkedIn</a></li>
+            <li><a about="blank" href="https://github.com/VictorHugoBatista">GitHub</a></li>
+          </ul>
         </footer>
       </main>
     </div>
