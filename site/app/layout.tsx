ebb03b73a81acb43a8c4d9949d54f3cb6000180c
Victor Hugo Batista <vct.bta@gmail.com>
2025-10-02 18:34:24 -0300

    Add main page style and title

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="monitor">
+          <div className="monitor-screen">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
