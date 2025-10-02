6a4d83dbf4599d0d2b5e534c660f3325d33a2655
Victor Hugo Batista <vct.bta@gmail.com>
2025-10-02 18:47:36 -0300

    Add body style

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,6 +1,10 @@
-import { Bitcount } from 'next/font/google';
+import { Bitcount, Bitcount_Single } from 'next/font/google';
 
 const _fontTitle = Bitcount({
+  subsets: ['latin'],
+});
+
+const _fontPage = Bitcount_Single({
   subsets: ['latin'],
 })
 
@@ -8,10 +12,20 @@ export default function Home() {
   return (
     <div className="container mx-auto px-4">
       <main>
-        <section className="monitor-screen-title">
+        <header className="monitor-screen-title">
           <h1>Victor Hugo Batista</h1>
-          <hr />
+        </header>
+        <section className="monitor-screen-content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero pariatur dolor maiores a nesciunt autem reprehenderit maxime voluptates quidem id deserunt odio, ipsum officiis fuga, aperiam nobis aliquid repudiandae officia.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero pariatur dolor maiores a nesciunt autem reprehenderit maxime voluptates quidem id deserunt odio, ipsum officiis fuga, aperiam nobis aliquid repudiandae officia.
+          </p>
         </section>
+        <footer className="monitor-screen-footer">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero pariatur dolor maiores a nesciunt autem reprehenderit maxime voluptates quidem id deserunt odio, ipsum officiis fuga, aperiam nobis aliquid repudiandae officia.
+        </footer>
       </main>
     </div>
   );
